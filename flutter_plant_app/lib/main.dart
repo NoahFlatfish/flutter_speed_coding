ae6aabb71fa64f668c3021586ad775584b4f3b36
mrnoah9706 <81079355+mrnoah9706@users.noreply.github.com>
2021-08-30 11:12:01 +0900

    project-setup

---
+++ flutter_plant_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_plant_app/components/home_screen.dart';
+import 'package:flutter_plant_app/constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
