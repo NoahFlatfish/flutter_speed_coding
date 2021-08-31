ea53d0c0dfe1219da4640e53963e421b87246ce0
mrnoah9706 <81079355+mrnoah9706@users.noreply.github.com>
2021-08-31 17:17:11 +0900

    파일 재정리

--- flutter_plant_app/lib/main.dart
+++ flutter_plant_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_plant_app/home/home_screen.dart';
 import 'package:flutter_plant_app/constants.dart';
+import 'package:flutter_plant_app/screens/home/home_screen.dart';
 
 void main() {
   runApp(MyApp());
--- flutter_plant_app/lib/screens/home/home_screen.dart
+++ flutter_plant_app/lib/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_plant_app/compoonents/bttm_nv_bar.dart';
-import 'package:flutter_plant_app/home/components/body.dart';
+import 'package:flutter_plant_app/screens/home/components/body.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
